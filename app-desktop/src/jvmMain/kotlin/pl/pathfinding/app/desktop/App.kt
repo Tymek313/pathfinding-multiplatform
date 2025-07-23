bdf609755d3acaa85d55ef36aa8b74a5570ae66d
Tymek313 <236069118+Tymek313@users.noreply.github.com>
2025-07-23 02:27:20 +0200

    Set minimum window size for desktop

--- app-desktop/src/jvmMain/kotlin/pl/pathfinding/app/desktop/App.kt
+++ app-desktop/src/jvmMain/kotlin/pl/pathfinding/app/desktop/App.kt
@@ -3,35 +3,44 @@ package pl.pathfinding.app.desktop
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowPosition
-import androidx.compose.ui.window.application
-import androidx.compose.ui.window.rememberWindowState
-import org.jetbrains.compose.resources.stringResource
+import androidx.compose.ui.window.WindowState
+import androidx.compose.ui.window.singleWindowApplication
+import kotlinx.coroutines.runBlocking
+import org.jetbrains.compose.resources.getString
 import pathfinding.shared.ui.generated.resources.Res
 import pathfinding.shared.ui.generated.resources.graph_pathfinding
 import pl.pathfinding.shared.ui.screen.BoardScreen
 import pl.pathfinding.shared.ui.theme.PathfindingBackground
 import pl.pathfinding.shared.ui.theme.PathfindingTheme
+import java.awt.Dimension
 import java.io.FileWriter
+import java.io.PrintStream
+import java.io.PrintWriter
 
 fun main() {
+    registerGlobalExceptionHandler()
+    singleWindowApplication(
+        state = WindowState(width = 1200.dp, height = 700.dp, position = WindowPosition.Aligned(Alignment.Center)),
+        title = runBlocking { getString(Res.string.graph_pathfinding) }
+    ) {
+        val density = LocalDensity.current
+        LaunchedEffect(Unit) {
+            window.minimumSize = density.run { Dimension(500.dp.roundToPx(), 500.dp.roundToPx()) }
+        }
+        App()
+    }
+}
+
+private fun registerGlobalExceptionHandler() {
     Thread.setDefaultUncaughtExceptionHandler { _, throwable ->
         throwable.printStackTrace()
-        FileWriter("error-log.txt").use { writer ->
-            writer.write(throwable.stackTraceToString())
-        }
-    }
-    application {
-        Window(
-            onCloseRequest = ::exitApplication,
-            state = rememberWindowState(width = 1200.dp, height = 900.dp, position = WindowPosition.Aligned(Alignment.Center)),
-            title = stringResource(Res.string.graph_pathfinding)
-        ) {
-            App()
-        }
+        // For debugging release builds
+        PrintWriter("error-log.txt").use(throwable::printStackTrace)
     }
 }
 
